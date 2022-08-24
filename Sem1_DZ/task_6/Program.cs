ab027834ede79102b64642547e2f0a533a3cc030
Anatoly Kirillov <kirillovtolia@gmail.com>
2022-08-24 12:36:55 +0300

    add task_6_project

---
+++ Sem1_DZ/task_6/Program.cs
@@ -0,0 +1,11 @@
+﻿// Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).
+Console.WriteLine("Напишите число: ");
+double number1 = double.Parse(Console.ReadLine());
+if(number1 % 2 == 0)
+{
+    Console.WriteLine($"Указанное число: {number1} - чётное");
+}
+else
+{
+    Console.WriteLine($"Указанное число: {number1} - не чётное");
+}
