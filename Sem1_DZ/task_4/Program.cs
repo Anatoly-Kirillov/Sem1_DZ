0c9dc7e7c8c4cea52e596e6aac628209b8d21d42
Anatoly Kirillov <kirillovtolia@gmail.com>
2022-08-24 12:17:40 +0300

    DZ task_4_project

---
+++ Sem1_DZ/task_4/Program.cs
@@ -0,0 +1,19 @@
+﻿// Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+Console.WriteLine("Напишите первое число: ");
+double num1 = double.Parse(Console.ReadLine());
+Console.WriteLine("Напишите второе число: ");
+double num2 = double.Parse(Console.ReadLine());
+Console.WriteLine("Напишите третье число: ");
+double num3 = double.Parse(Console.ReadLine());
+
+double max = num1;
+
+if(num2 > max)
+{
+    max = num2;
+}
+if(num3 > max)
+{
+    max = num3;
+}
+Console.WriteLine($"Максимальное число: {max}");
