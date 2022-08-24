d1032a7177c3edc0ddda9b34a6d49d4c89138b14
Anatoly Kirillov <kirillovtolia@gmail.com>
2022-08-24 14:15:26 +0300

    Add task_8_project

---
+++ Sem1_DZ/task_8/Program.cs
@@ -0,0 +1,9 @@
+﻿// Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+Console.WriteLine("Напишите число: ");
+double num1 = double.Parse(Console.ReadLine());
+double N = 2;
+while (N <= num1)
+{
+    Console.Write($"{N} ");
+    N = N + 2;
+}
